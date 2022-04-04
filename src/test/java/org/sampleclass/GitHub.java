38a6f9ebe9f95c6c474e551bdc4e4a0618e01984
vasanth8888 <vasanthveeda09@gmail.com>
2022-04-04 12:08:19 +0530

    my task is done

---
+++ src/test/java/org/sampleclass/GitHub.java
@@ -0,0 +1,25 @@
+package org.sampleclass;
+
+public class GitHub {
+	
+	
+	
+	private void method1() {
+		
+		System.out.println("one");
+	}
+	
+	
+	private void method2() {
+		System.out.println("two");
+
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
